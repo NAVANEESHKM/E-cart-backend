bf53c024c1d6695bb6bbd6e59bf33dd30c690f2d
NAVANEESH K M <navaneesh.ee20@bitsathy.ac.in>
2023-11-21 11:11:06 +0530

    u

--- gettingall.js
+++ gettingall.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const router1 = express.Router();
-const Item = require('./schemes'); // Import your Item model
+// const express = require('express');
+// const router1 = express.Router();
+// const Item = require('./schemes'); // Import your Item model
 
-// Define the route to retrieve an item by ID
-router1.get('/all', async (req, res) => {
-  try {
-    const itemId = req.body;
+// // Define the route to retrieve an item by ID
+// router1.get('/all', async (req, res) => {
+//   try {
+//     const itemId = req.body;
 
-    const item = await Item.find({});
+//     const item = await Item.find({});
 
-    if (!itemId) {
-      return res.status(404).json({ message: 'Item not found' });
-    }
+//     if (!itemId) {
+//       return res.status(404).json({ message: 'Item not found' });
+//     }
 
-    res.status(200).json(item); // Send the retrieved item as the response
-  } catch (error) {
-    console.error('Error retrieving item:', error);
-    res.status(500).json({ error: 'An error occurred while retrieving the item' });
-  }
-});
+//     res.status(200).json(item); // Send the retrieved item as the response
+//   } catch (error) {
+//     console.error('Error retrieving item:', error);
+//     res.status(500).json({ error: 'An error occurred while retrieving the item' });
+//   }
+// });
 
-module.exports = router1;
+// module.exports = router1;
