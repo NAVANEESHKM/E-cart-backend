79ea3d5c9e9a37c0bef1b10efc65a6bf1f6ec781
NAVANEESH K M <navaneesh.ee20@bitsathy.ac.in>
2024-01-26 19:31:15 +0530

    u

--- productget.js
+++ productget.js
@@ -2,19 +2,37 @@ const express = require('express');
 const router1 = express.Router();
 const Item = require('./ProductSchema');
 
-// Define the route to retrieve an item by ID
 router1.post('/all/productget', async (req, res) => {
   try {
-    const item= await Item.collection.find({}).toArray();
-  
-    
+    // Retrieve items from the database
+    const items = await Item.find();
 
-    res.status(200).json(item); // Send the retrieved item as the response
-  }catch (error) {
-    console.error('Error retrieving item:', error);
-    res.status(500).json({ error: 'An error occurred while retrieving the item' });
+    // Map the items to include the image data
+    const itemsWithImages = items.map(item => {
+      const images = item.image || []; // Assuming 'image' is an array
+
+      // Map the images in the array
+      const mappedImages = images.map(img => {
+        return {
+          data: img.data.toString('base64'), // Convert Buffer to base64
+          contentType: img.contentType
+        };
+      });
+
+      return {
+        productname: item.productname,
+        benefits: item.benefits,
+        unit: item.unit,
+        price: item.price,
+        images: mappedImages
+      };
+    });
+
+    res.status(200).json(itemsWithImages); // Send the retrieved items with images as the response
+  } catch (error) {
+    console.error('Error retrieving items:', error);
+    res.status(500).json({ error: 'An error occurred while retrieving the items' });
   }
 });
 
 module.exports = router1;
-
